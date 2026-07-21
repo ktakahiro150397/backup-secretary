d2ab28a8d3f9f7ed2134a2fcb7fcd7b15860ce09
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2026-07-21 19:54:38 +0900

    fix: expose bundled plugin to image test

--- scripts/test-hermes-otel-image.sh
+++ scripts/test-hermes-otel-image.sh
@@ -9,6 +9,7 @@ docker run --rm -i \
   --tmpfs /opt/testhome:rw,nosuid,size=32m,uid=1000,gid=1000,mode=0700 \
   -e HOME=/opt/testhome \
   -e HERMES_HOME=/opt/testhome \
+  -e PYTHONPATH=/opt/hermes/plugins \
   -v "${repo_dir}/observability/hermes-otel/main.yaml:/run/main.yaml:ro" \
   -v "${repo_dir}/observability/hermes-otel/owashota.yaml:/run/owashota.yaml:ro" \
   --entrypoint bash \
